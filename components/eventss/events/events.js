73ffacf6000a02471bb431a301615c12413c6808
LechuMamais <119885029+LechuMamais@users.noreply.github.com>
2024-04-08 20:22:51 +0200

    Improved footer!

--- components/eventss/events/events.js
+++ components/eventss/events/events.js
@@ -23,40 +23,6 @@ export const showEvents = async () => {
         
         eventsContainer.appendChild(eventsCardContainer);
 
-        
-
-        //  -----------------------------------     EVENTS CONTAINER MODIFIER     -------------------------------- //
-
-        // Si a la vez que se visualiza el hero también hay un events container debajo, vamos a darle margen suficiente para que quepa
-
-        /*var heroContainerPreviousHeight
-        // Funcion de ajustarle el margin top al eventsContainer para que se ubique debajo del hero.
-        const eventsContainerMarginTop = () => {
-            //console.log('Modificando events container margin top')
-            const heroContainer = document.querySelector('.hero-container');
-            const heroContainerActualHeight = heroContainer.offsetHeight;
-            const headerHeight = document.querySelector('header').offsetHeight;
-            const headerAndHeroHeight = heroContainerActualHeight + headerHeight;
-            const scrollPosition = window.scrollY;
-            const adjustParams = -60;
-
-            if(heroContainerActualHeight < heroContainerPreviousHeight){
-                eventsContainer.style.marginTop = `${headerAndHeroHeight+scrollPosition+adjustParams}px`;
-            }else if(heroContainerActualHeight > heroContainerPreviousHeight){
-                eventsContainer.style.marginTop = `${headerAndHeroHeight+scrollPosition+adjustParams}px`;
-            }
-
-            heroContainerPreviousHeight= heroContainerActualHeight
-        }
-
-        // Que eventsContainerMarginTop se ejecute solo si está cargado eventsContainer, y si hay tambien heroContainer visualizandose.
-        window.addEventListener('scroll', () => {
-            const heroContainer = document.querySelector('.hero-container');
-            if (eventsContainer && heroContainer) {
-                eventsContainerMarginTop();
-            }
-        });*/
-
         // Agregar el contenedor de eventos al DOM
         return eventsContainer;
 
--- components/global/footer/footer.js
+++ components/global/footer/footer.js
@@ -3,9 +3,29 @@ import "./footer.css";
 export const createFooter = () => {
     const footer = document.createElement("footer");
     footer.innerHTML = `
-        <ul id="footer-list">
-            <h4>>Web app created by Alexis Mamais<</h4>
+    <div id="footer-lists-container">
+        <ul class="footer-list footer-list-top">
+            <li>
+                <a href="https://www.instagram.com" target="_blank"><i class="bi bi-instagram"></i></a>
+            </li>
+            <li>
+                <a href="https://www.facebook.com" target="_blank"><i class="bi bi-twitter-x"></i></a>
+            </li>
+            <li>
+                <a href="https://www.twitter.com" target="_blank"><i class="bi bi-facebook"></i></a>
+            </li>
         </ul>
+        <ul class="footer-list footer-list-bottom">
+            <li id="footer-contacto">
+                <p>Contacto: <a href="mailto:alexismamais@gmail.com">alexismamais@gmail.com</a> | Teléfono: +123456789</p>
+            </li id="footer-legal">
+                <p>&copy; 2024 Insomnia Events Manager.</p>
+                <p>Todos los derechos reservados</p>
+                <p>Desarrollado por <a href="https://www.desarrollador.com" target="_blank">Alexis Mamais</a></p>
+            <li>
+            </li>
+        </ul>
+    </div>
     `;
     return footer
 }
